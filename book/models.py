4e0a31a649ff45d9c7419f7922f9b95059519d67
Ira <iramladanovych@gmail.com>
2024-10-03 18:19:45 +0300

    Create book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=255)
+    inventory = models.IntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)

ebeb5cc0b67ef80162d5ee0508ad9abc52269d3b
Ira <iramladanovych@gmail.com>
2024-10-04 09:43:37 +0300

    Create persmissions, create models Genre & Author, update serializers

--- book/models.py
+++ book/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
+    class StatusChoices(models.TextChoices):
+        SOFT = "Soft"
+        HARD = "Hard"
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    cover = models.CharField(max_length=255)
-    inventory = models.IntegerField()
+    genres = models.ManyToManyField(Genre)
+    author = models.ManyToManyField(Author)
+    cover = models.CharField(max_length=255, choices=StatusChoices.choices)
+    inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(null=True, upload_to="movie_image_path")
+
+    def __str__(self):
+        return f" {self.title} {self.author}"
+
+
--- book/serializers.py
+++ book/serializers.py
@@ -2,10 +2,77 @@ from rest_framework import serializers
 
 from book.models import (
     Book,
+    Genre,
+    Author,
 )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ("id", "first_name", "last_name", "full_name")
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ("title", "author", "cover", "inventory", "daily_fee")
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee",
+            "image"
+        )
+        read_only_fields = ("image",)
+
+
+class BookListSerializer(BookSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+    authors = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee",
+            "image"
+        )
+
+
+class BookDetailSerializer(BookSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    authors = AuthorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Book
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee",
+            "image"
+        )
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "image")

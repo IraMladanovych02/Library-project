96d04f25c5c21d62e4912c246272dc8b803a6f60
Ira <iramladanovych@gmail.com>
2024-10-04 09:55:20 +0300

    Added genres and authors to views and urls

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path, include
+
 from rest_framework import routers
 
 from book.views import (
+    GenreViewSet,
+    AuthorViewSet,
     BookViewSet,
 )
 
@@ -8,6 +12,8 @@ app_name = "book"
 
 router = routers.DefaultRouter()
 
+router.register("genres", GenreViewSet)
+router.register("authors", AuthorViewSet)
 router.register("books", BookViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [path("", include(router.urls))]
--- book/views.py
+++ book/views.py
@@ -1,18 +1,94 @@
-from rest_framework import mixins
+from rest_framework import mixins, status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-from book.models import Book
-from book.serializers import BookSerializer
+from book.models import Book, Genre, Author
+from book.permissions import IsAdminOrIfAuthenticatedReadOnly
+from book.serializers import BookSerializer, GenreSerializer, AuthorSerializer, BookListSerializer, \
+    BookDetailSerializer, ImageSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class AuthorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class BookViewSet(
-    mixins.ListModelMixin,
+    ReadOnlyModelViewSet,
     mixins.CreateModelMixin,
-    GenericViewSet
+    GenericViewSet,
 ):
-    queryset = Book.objects.all()
+    queryset = Book.objects.prefetch_related("genres", "authors")
     serializer_class = BookSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the books with filters"""
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        authors = self.request.query_params.get("authors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if authors:
+            authors_ids = self._params_to_ints(authors)
+            queryset = queryset.filter(authors__id__in=authors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        if self.action == "retrieve":
+            return BookDetailSerializer
+        if self.action == "upload_image":
+            return ImageSerializer
+
+        return BookSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser, ],
+    )
+    def upload_image(self, request, pk=None):
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
